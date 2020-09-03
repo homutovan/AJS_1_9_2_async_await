49be0a866f146472486c16d91b8bca8f142129b9
Andrey Khomutov <homutovan@live.com>
2020-09-04 01:00:07 +0700

    fix bug

--- src/js/__tests__/gamesavingloader.test.js
+++ src/js/__tests__/gamesavingloader.test.js
@@ -2,11 +2,16 @@ import GameSavingLoader from '../gamesavingloader';
 import { saveData } from '../reader';
 import GameSaving from '../gamesaving';
 
+const testData = '{"id": 9,"created": 1546300800, "userInfo": {"id": 1, "name": "Rembo", "level": 10, "points":2000}}';
 const hitman = new GameSaving(JSON.parse(saveData));
+const rembo = new GameSaving(JSON.parse(testData));
 
-test('Test case №1', (done) => {
-  GameSavingLoader.load().then((result) => {
-    expect(result).toEqual(hitman);
-  }).catch((err) => err);
-  done();
+test('Test case №1', async () => {
+  const save = await GameSavingLoader.load();
+  expect(save).toEqual(hitman);
+});
+
+test('Test case №2', async () => {
+  const save = await GameSavingLoader.load();
+  expect(save).not.toEqual(rembo);
 });
